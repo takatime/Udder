3b6b613f030e88a1d4e637d7b4cbeec13200e36a
Taka Suzuki <ooo13@Takas-MacBook-Pro.local>
2018-11-16 13:51:13 -0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Taka Suzuki on 11/16/18.
+//  Copyright © 2018 Taka Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
